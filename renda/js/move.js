e793c3f84d18256389145ca5e5ff4ace212f5d55
webbasedAR <webbasedar@protonmail.com>
2020-05-02 13:53:55 +0200

    fix width and height AR frame

---
+++ renda/js/move.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+
+
+
+    var newParent = document.getElementById('arjs-video');
+    var oldParent = document.getElementById('nft');
+    
+    while (oldParent.childNodes.length > 0) {
+        newParent.appendChild(oldParent.childNodes[0]);
+    }
+
+}
